1c49318535c0a4b83f3a51fdb62870994b45588a
Johnny <johnny@jober.io>
2018-04-15 01:25:53 +0900

    update header text

--- src/component/Header.js
+++ src/component/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
       <Jumbotron fluid className="header-main-component">
         <div className="header-main-title">오늘도 바쁘신가요?</div>
         <div className="header-body-text">
-          <div>자꾸만 미루지 마시고 바로 오늘</div>
+          <div>자꾸만 미루지 말고 바로 오늘</div>
           <div>사랑하는 사람에게 마음을 전달하세요</div>
         </div>
       </Jumbotron>
