66aeac14e7d963726f1c2f71f4b2849f0440d5da
Johnny <johnny@jober.io>
2018-04-14 12:14:33 +0900

    font size update

--- src/component/Body.js
+++ src/component/Body.js
@@ -29,20 +29,22 @@ class Body extends Component {
     return (
       <Container fluid className="body-main-component">
         <Row>
-          <Col lg="6" className="body-description">
-            <div className="body-description-title">꽃으로 사랑을 표현 해보세요</div>
+          <Col lg="8" className="body-description">
+            <div className="body-description-title">꽃으로 사랑을 전달하세요</div>
             <div className="body-description-detail">
-              <li>도매시장 직배송에 의한 상상 그 이상의 싱싱함</li>
-              <li>기존 꽃집과 비교해서 말도 안 되는 장미의 풍성함</li>
-              <li>무료 고급 꽃바구니 + 무료 배송!</li>
-              <li>원하시는 시간과 장소로 배송해 드립니다</li>
+              <div className="body-description-detail-inner">
+                <li>도매시장 직배송에 의한 상상 그 이상의 싱싱함</li>
+                <li>기존 꽃집과 비교해서 말도 안 되는 장미의 풍성함</li>
+                <li>무료 고급 꽃바구니 + 무료 배송!</li>
+                <li>원하시는 시간과 장소로 배송해 드립니다</li>
+              </div>
             </div>
             <div className="body-description-price">가격 50,000 원 (바구니, 장식, 퀵배송 포함)</div>
             <div className="open-modal order-button-div">
               <Button className="order-button" onClick={this.onOpenOrderModal}>주문하기</Button>
             </div>
           </Col>
-          <Col lg="6" className="body-image">
+          <Col lg="4" className="body-image">
             <div className="flower-image-container">
               <img className="flower-image" alt="flower" src={require('../img/flower.png')} />
             </div>
--- src/component/Header.js
+++ src/component/Header.js
@@ -5,9 +5,9 @@ class Header extends Component {
   render() {
     return (
       <Jumbotron fluid className="header-main-component">
-        <div className="header-main-title">쿠스랩 = 바쁜 직장인을 위한 솔루션</div>
+        <div className="header-main-title">바쁜 직장인을 위한 선물가게</div>
         <div className="header-body-text">
-          <span>계속되는 야근에 회식까지.. 많이 바쁘시죠? 바쁜 당신을 대신해서 사랑하는 사람에게 행복을 전해드립니다.</span>
+          <span>많이 바쁘시죠? 바쁜 당신을 대신해서 사랑하는 사람에게 행복을 전해드립니다.</span>
         </div>
       </Jumbotron>
     );
