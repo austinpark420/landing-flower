dca75a20a0a85ff0264af50bd95336f8a1f781b9
Minhoko <minoerick120@gmail.com>
2018-04-23 21:36:08 +0700

    update: header style in mobile size is revised

--- src/component/Header.js
+++ src/component/Header.js
@@ -5,10 +5,16 @@ class Header extends Component {
   render() {
     return (
       <Jumbotron fluid className="header-main-component">
-        <div className="header-main-title">오늘도 바쁘신가요?</div>
+        <div className="header-main-title">
+          <span className="first-title">오늘도 </span>
+          <span className="second-title">바쁘신가요?</span>
+        </div>
         <div className="header-body-text">
           <div>자꾸만 미루지 말고 바로 오늘</div>
-          <div>사랑하는 사람에게 마음을 전달하세요</div>
+          <div>
+            <span>사랑하는 사람에게 마음을 </span>
+            <span className="last-body-text">전달하세요</span>
+          </div>
         </div>
       </Jumbotron>
     );
