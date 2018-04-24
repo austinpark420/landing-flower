bfec8ffd82ee1e2c20882777606b2b3f36105053
Johnny <johnny@jober.io>
2018-04-25 01:45:32 +0900

    update layout

--- src/component/Body.js
+++ src/component/Body.js
@@ -32,21 +32,21 @@ class Body extends Component {
           <Col lg="6" className="body-description">
             <div className="body-description-title">꽃바구니로</div>
             <div className="body-description-title">마음을 전달하세요</div>
+            <div className="body-description-price">
+              <span className="price-text">가격 <p className="amount">50,000</p>원</span>
+            </div>
             <div className="body-description-detail">
               <ListGroup className="body-description-detail-inner">
-                <ListGroupItem>도매시장 직배송으로 최상의 싱싱함</ListGroupItem>
-                <ListGroupItem>기존 온라인/꽃집과 비교하여 2배 이상의 풍성함</ListGroupItem>
+                <ListGroupItem>도매시장 직배송으로 싱싱함 보장</ListGroupItem>
+                <ListGroupItem>기존 꽃집 대비 2배 이상의 풍성함</ListGroupItem>
                 <ListGroupItem>꽃바구니 + 배송비 포함 가격</ListGroupItem>
                 <ListGroupItem>원하시는 시간과 장소로 배송해 드려요</ListGroupItem>
               </ListGroup>
             </div>
-            <div className="body-description-price">
-              <span className="price-text">가격 <p className="amount">50,000</p>원</span>
-            </div>
             <ListGroup className="body-description-caution">
-              <ListGroupItem>현재 서울지역만 주문을 받고 있습니다.</ListGroupItem>
-              <ListGroupItem>수도권 지역은 별도로 연락 주시기 바랍니다.</ListGroupItem>
-              <ListGroupItem>배송 24시간 전에는 주문이 필요합니다.</ListGroupItem>
+              <ListGroupItem>현재 서울 지역만 배송</ListGroupItem>
+              <ListGroupItem>수도권 지역은 별도상담</ListGroupItem>
+              <ListGroupItem>배송 24시간 전 주문 필요</ListGroupItem>
             </ListGroup>
             <div className="open-modal order-button-div">
               <Button className="order-button" size="lg" onClick={this.onOpenOrderModal}>마음 보내기</Button>
