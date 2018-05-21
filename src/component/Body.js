1b23051ae134ae24de4dfc191111441c6cec0b51
austinpark420 <austin.park420@gmail.com>
2018-05-21 17:07:53 +0900

    add publicising branch changed html, css and i only modified below 455px(width) layout.

--- src/component/Body.js
+++ src/component/Body.js
@@ -29,33 +29,31 @@ class Body extends Component {
     return (
       <Container fluid className="body-main-component">
         <Row>
-          <Col lg="6" className="body-description">
-            <div className="body-description-title">꽃바구니로</div>
-            <div className="body-description-title">마음을 전달하세요</div>
-            <div className="body-description-price">
-              <span className="price-text">가격 <p className="amount">50,000</p>원</span>
-            </div>
-            <div className="body-description-detail">
-              <ListGroup className="body-description-detail-inner">
-                <ListGroupItem>도매시장 직배송으로 싱싱함 보장</ListGroupItem>
-                <ListGroupItem>기존 꽃집 대비 2배 이상의 풍성함</ListGroupItem>
-                <ListGroupItem>꽃바구니 + 배송비 포함 가격</ListGroupItem>
-                <ListGroupItem>원하시는 시간과 장소로 배송해 드려요</ListGroupItem>
-              </ListGroup>
-            </div>
-            <ListGroup className="body-description-caution">
-              <ListGroupItem>현재 서울 지역만 배송</ListGroupItem>
-              <ListGroupItem>수도권 지역은 별도상담</ListGroupItem>
-              <ListGroupItem>배송 24시간 전 주문 필요</ListGroupItem>
-            </ListGroup>
-            <div className="open-modal order-button-div">
-              <Button className="order-button" size="lg" onClick={this.onOpenOrderModal}>마음 보내기</Button>
-            </div>
-          </Col>
           <Col lg="6" className="body-image">
             <div className="flower-image-container">
               <img className="flower-image" alt="flower" src={require('../img/flower.png')} />
             </div>
+          </Col>
+          <Col lg="6" className="body-description">
+            <div className="body-description-title">진심을 담은</div>
+            <div className="body-description-title body-description-main-title">연분홍 장미바구니</div>
+            <div className="body-description-price">
+              <span className="price-text">50,000 <p className="amount">원</p></span>
+            </div>
+            <div className="open-modal order-button-div">
+              <Button className="order-button" size="lg" onClick={this.onOpenOrderModal}>Order</Button>
+            </div>
+            <div className="body-description-detail">
+              <ListGroup className="body-description-detail-inner">
+                <ListGroupItem>도매시장 직배송으로 싱싱함</ListGroupItem>
+                <ListGroupItem>일반 꽃집 대비 2배 이상의 풍성함</ListGroupItem>
+                <ListGroupItem>원하시는 시간과 장소로 배송해 드려요</ListGroupItem>
+              </ListGroup>
+            </div>
+            <ListGroup className="body-description-caution">
+              <ListGroupItem>서울 지역만 배송(그외 지역은 별도상담)</ListGroupItem>
+              <ListGroupItem>배송 24시간 전 주문 필요</ListGroupItem>
+            </ListGroup>
           </Col>
         </Row>
       </Container>
