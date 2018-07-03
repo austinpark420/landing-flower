1ea355cd0cc3f16720025d40008274eb38b93d7a
austinpark420 <austin.park420@gmail.com>
2018-07-03 19:40:16 +0900

    edit publishing/main

--- src/component/fields/nameField.js
+++ src/component/fields/nameField.js
@@ -5,7 +5,7 @@ const nameField = ({ input, placeholder, type, meta: { touched, error } }) => {
 
   return (
     <InputGroup>
-      <InputGroup validationState={touched && error ? 'error' : ''}>
+      <InputGroup validationstate={touched && error ? 'error' : ''}>
         <Input {...input} placeholder={placeholder} type={type} style={{ height: '1.8rem' }} />
         {/* {touched && error && <span style={{ color: 'rgb(121, 46, 46)' }}>이름을 입력해주세요!</span>} */}
       </InputGroup>
